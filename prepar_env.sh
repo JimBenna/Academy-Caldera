0679075c9723100413e2db2111f12b288a1669cd
JimBenna <jim.benna@gmail.com>
2022-08-04 15:53:59 +0200

    script modifications

--- prepar_env.sh
+++ prepar_env.sh
@@ -4,13 +4,14 @@ while true; do
 read -p "This script prepare the working environment.
 Are you sure you want to do that ? (y/n) ?"  yn
 		case $yn in
-		[yY]* ) echo "Ready to go"; 
-			mkdir ~/caldera/data/abilities &>/dev/null;
-			cp -r ~/repo/abilities ~/caldera/data;
-			mkdir ~/caldera/data/adversaries &>/dev/null;
-			cp -r ~/repo/adversaries ~/caldera/data;
+		[yY]* ) mkdir ~/caldera/data/abilities >/dev/null 2>&1;
+			cp -r ~/repo/abilities ~/caldera/data >/dev/null 2>&1;
+			mkdir ~/caldera/data/adversaries >/dev/null 2>&1;
+			cp -r ~/repo/adversaries ~/caldera/data >/dev/null 2>&1;
+			echo "\e[35mAll is now ready to launch a \e[32mfunny game \e[1;36m;-)";
 			break;; 
 		[nN]* ) echo "Cancelled"; exit ;;
 		* ) clear; echo; echo "Invalid choice, select yes or no";;
 esac
 done
+
