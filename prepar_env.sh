5b6ace88bb620a8f19063c2846b8763b24cc84a0
JimBenna <110407297+JimBenna@users.noreply.github.com>
2022-08-04 16:00:05 +0200

    Correct script color error

--- prepar_env.sh
+++ prepar_env.sh
@@ -8,7 +8,7 @@ Are you sure you want to do that ? (y/n) ?"  yn
 			cp -r ~/repo/abilities ~/caldera/data >/dev/null 2>&1;
 			mkdir ~/caldera/data/adversaries >/dev/null 2>&1;
 			cp -r ~/repo/adversaries ~/caldera/data >/dev/null 2>&1;
-			echo "\e[35mAll is now ready to launch a \e[32mfunny game \e[1;36m;-)";
+			echo "\e[35mAll is now ready to launch a \e[32mfunny game \e[1;36m;-)\e[0;37m";
 			break;; 
 		[nN]* ) echo "Cancelled"; exit ;;
 		* ) clear; echo; echo "Invalid choice, select yes or no";;
